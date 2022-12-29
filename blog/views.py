bd466b379e2834d2fd67c6f04d085e1b28acafc1
Axel Ef <olek4don@gmail.com>
2022-12-29 06:30:12 +0100

    create base.html and make some basic inheritance in templates/

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse  # TODO: delete it
+
+posts = [
+    {
+        'author': 'AxelEf',
+        'title': 'Blog Post 1',
+        'content': 'Content 1',
+        'date_posted': 'December 23, 2022'
+    },
+    {
+        'author': 'Ada J',
+        'title': 'Blog Post 2',
+        'content': 'Content 2',
+        'date_posted': 'December 24, 2022'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+    # return HttpResponse('<h1>Blog Home</h1>')  # TODO: DELETE IT
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
-    
+    return render(request, 'blog/about.html', {'title': 'About Blog'})
