1fe7d58a335cbb7538357e9473322caae0ff0ef3
Axel Ef <olek4don@gmail.com>
2023-01-02 20:07:12 +0100

    modify views and admin.py thast we can access to GUI on blog-webpage. Add proper date format for posts in home.html

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse  # TODO: delete it
 
-posts = [
-    {
-        'author': 'AxelEf',
-        'title': 'Blog Post 1',
-        'content': 'Content 1',
-        'date_posted': 'December 23, 2022'
-    },
-    {
-        'author': 'Ada J',
-        'title': 'Blog Post 2',
-        'content': 'Content 2',
-        'date_posted': 'December 24, 2022'
-    }
-]
+# posts = [
+#     {
+#         'author': 'AxelEf',
+#         'title': 'Blog Post 1',
+#         'content': 'Content 1',            # TODO: delete whole posts
+#         'date_posted': 'December 23, 2022' # no longer need after modify 'posts' in context
+#     },                                     # and since we have set SQL connection with database
+#     {                                      # in Django shell
+#         'author': 'Ada J',
+#         'title': 'Blog Post 2',
+#         'content': 'Content 2',
+#         'date_posted': 'December 24, 2022'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
     # return HttpResponse('<h1>Blog Home</h1>')  # TODO: DELETE IT
