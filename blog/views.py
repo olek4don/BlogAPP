3293fbaddc66d2c545e9bf63953d3f92adbed69b
Axel Ef <olek4don@gmail.com>
2023-01-23 21:23:39 +0100

    The rest of getting absolute URL (using login mixin, creating an update view, preventing of update post by strangers with UserPassesTesMixin).

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView,
 )
 from .models import Post
 # from django.http import HttpResponse  # TODO: delete it
@@ -42,7 +44,16 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
